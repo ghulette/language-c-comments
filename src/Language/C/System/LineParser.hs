2c1a1b2d1b791981747a7f4acb60bf2256988cad
Geoff Hulette <ghulette@gmail.com>
2010-10-04 17:37:00 -0500

    Added line joins

---
+++ src/Language/C/System/LineParser.hs
@@ -0,0 +1,41 @@
+module LineParser (parseLines) where
+import Data.List
+
+isWhitespace :: Char -> Bool
+isWhitespace ' ' = True
+isWhitespace '\t' = True
+isWhitespace _ = False
+
+stripLeadingWhitespace = dropWhile isWhitespace
+stripTrailingWhitespace = reverse . stripLeadingWhitespace . reverse
+dropLast = reverse . tail . reverse
+endsWith = \c -> (startsWith c) . reverse
+
+startsWith :: Eq a => a -> [a] -> Bool
+startsWith _ [] = False
+startsWith x (y:_) = x == y
+
+strip :: String -> String
+strip s = if isBrokenLine s then dropLast (stripTrailingWhitespace s) else s
+  
+isBrokenLine :: String -> Bool
+isBrokenLine = (endsWith '\\') . stripTrailingWhitespace
+
+joinBrokenLines :: [String] -> [String]
+joinBrokenLines [] = []
+joinBrokenLines [line] = [strip line]
+joinBrokenLines (line1:line2:rest) =
+  if isBrokenLine line1 then 
+    let joinedLine = (strip line1) ++ line2 in
+    joinBrokenLines (joinedLine:rest)
+  else 
+    line1:(joinBrokenLines (line2:rest))
+
+parseLines :: String -> [String]
+parseLines = joinBrokenLines . lines
+
+-- main :: IO ()
+-- main = do
+--   [file] <- getArgs
+--   fixed <- fmap parseLines (readFile file)
+--   mapM_ putStrLn fixed
